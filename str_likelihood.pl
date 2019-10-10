3bf7572d807f062545fa58411fcafa29918b28a3
Óscar Mira <oscarmirap@gmail.com>
2019-10-10 10:08:09 +0200

    Add files via upload
    Extract the likelihood values from the Structure outputs. It is an outdated script but it should do the work if you don't know another way

---
+++ str_likelihood.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+##Quick perl script to extract likelihood values from Structure output
+#It is outdated, now we use either a grep one liner or directly faststructure. -M'Ósky-
+
+my $OUTFOLDER="out";
+my $OUTDATA="out/results";
+my $outfile = "loglikelihood.txt";
+
+#Generating file with likelihoods
+
+
+print "Extracting the likelihood of each run\n";
+
+use Cwd qw(cwd);
+my $dir = cwd;
+
+opendir(my $DIR, $OUTDATA) or die "\nUnable to open directory $OUTDATA at $dir: $!\n";						#open the directory with the files
+my @files = readdir($DIR);					#extract filenames
+closedir($DIR);
+
+my $k = 0;
+
+my @saved_data =();
+my $fn =0;
+my $match = 0;
+my @wantedline = ();
+
+print "\nProcessing files from $OUTDATA:\n";
+foreach my $file (@files) {					#process all the files one by one
+	
+	next if ($file =~ /^\.$/);				#do not use hidden files
+	next if ($file =~ /^\.\.$/);			
+	next unless ($file =~ /.*_f$/);		#read only the _f files
+	print "$file  ";
+	my $filename = $file;
+	
+	my @runinfo = split('_' , $filename);
+	my $Kname = $runinfo[0];
+	my $iter = $runinfo[1];
+	my @karray = split(// , $Kname);
+	shift (@karray);
+	my $run_k = join ('' , @karray);
+	
+	my $filepath = "$dir"."/"."$OUTDATA"."/"."$file";
+	my $resultspath = "$dir"."/"."$OUTDATA"."/";
+	
+	open(my $IN, '<', $filepath) or die "error openning $file at $resultspath for reading: $!\n";
+	
+	while (<$IN>) {
+		if ( $_ =~ m/^Estimated Ln Prob of Data/ ) {
+			@wantedline = split(' ' , $_);
+			my $likelihood = $wantedline[-1];
+			$saved_data[$match] = "K\t$run_k\trep$iter\t$likelihood";
+			$match++;
+		}
+	}
+	$fn++;
+}
+
+
+print "\n\n$fn files processed, $match values saved.\n";
+
+my $outpath = "$OUTFOLDER"."/"."$outfile";
+
+open my $OUT, '>', $outpath or die "Cannot create output file $outpath: $!";
+
+# Loop over the array
+foreach (@saved_data) {print $OUT "$_\n";} # Print each entry in the array to the file
+
+close $OUT; 
+
+
+printf "\nLoglikelihood of each run saved to $outfile\n\n"
